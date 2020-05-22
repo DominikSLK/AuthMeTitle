9cc29cc09bec51fb5de45b85fcf00c689751ce37
IlyaHiguti <39450383+IlyaHiguti@users.noreply.github.com>
2020-05-22 09:03:09 +0800

    A bit optimization in TitleDescriptionException and add missing api-version, bump version

--- src/ru/ilyahiguti/authmetitle/title/TitleDescriptionException.java
+++ src/ru/ilyahiguti/authmetitle/title/TitleDescriptionException.java
@@ -1,10 +1,17 @@
 package ru.ilyahiguti.authmetitle.title;
 
 public class TitleDescriptionException extends Exception {
-    public TitleDescriptionException() {
-    }
-
     public TitleDescriptionException(String message) {
         super(message);
+    }
+
+    @Override
+    public synchronized Throwable initCause(Throwable cause) {
+        return this;
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
     }
 }
